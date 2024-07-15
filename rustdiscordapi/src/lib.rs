e4184f0a46c1c339e6cdbe316c87f1af1835efd8
Jacob Owens <owensinnovation@gmail.com>
2024-07-15 02:45:42 -0500

    Update lib.rs

--- rustdiscordapi/src/lib.rs
+++ rustdiscordapi/src/lib.rs
@@ -1,5 +1,9 @@
 pub mod utils;
 pub mod router;
+pub mod internal;
 
+include!(concat!(env!("OUT_DIR"), "/commands_mod.rs"));
+
+pub use internal::*;
 pub use utils::*;
 pub use router::{Command, CommandRouter};
